c189b789e1dc3159631a19aacc286a5b5827490c
Safayet <mdsafayet315@gmail.com>
2026-04-13 23:27:54 +0600

    SetUp Project & install all React Feacture

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Roots from './Router/Roots.jsx'
+import Home from './Pages/Home.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Roots,
+    children:[
+      {
+      index : true,
+      Component:Home
+    }
+  ]
+  },
+  {
+    path : "*",
+    element: <h1>This is erorafasjdfgasdf</h1>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
